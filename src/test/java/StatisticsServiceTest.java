53369258e70e99aa4c1dec4bba611d37e9df1825
diarytiger <diarytiger85@gmail.com>
2024-09-25 10:36:34 +0700

    Initial commit

---
+++ src/test/java/StatisticsServiceTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.stats.StatisticsService;
+
+public class StatisticsServiceTest {
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfFirstNotMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {3, 7, 9, 4, 5, 1, 9, 6, 15, 2, 1};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
